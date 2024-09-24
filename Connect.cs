61e44d581720422d38068d409c31fe3d0f4067d7
csanalosibalint <csanalosib@kkszki.hu>
2024-09-24 12:47:42 +0200

    Connect class is created.

---
+++ Connect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    public class Connect
+    {
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "Auto";
+            Username = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+        }
+    }
+}
